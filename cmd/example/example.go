5d68270dee3356893d73f9a56a82ea37fd01c0b8
Koelz Derrill J <koelz_derrill_j@cat.com>
2019-08-16 20:47:41 -0400

    Add initial implementation for waiter

--- cmd/example/example.go
+++ cmd/example/example.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/djkoelz/navi/pkg/comm"
+	"github.com/djkoelz/navi/pkg/control"
 	"github.com/djkoelz/navi/pkg/service"
+	"time"
 )
 
 func echo(m comm.Message) {
@@ -15,8 +17,20 @@ func main() {
 
 	m := NewStringMessage("hey, listen...")
 
+	waiter := control.NewWaiter(StringMessage{}.Desc())
+
 	dispatcher.Subscribe(StringMessage{}.Desc(), echo)
-	dispatcher.Post(m)
+
+	go func() {
+		if !waiter.Wait(dispatcher, 2*time.Second) {
+			fmt.Println("Timedout...")
+		}
+	}()
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		dispatcher.Post(m)
+	}()
 
 	for {
 	}
--- pkg/control/waiter.go
+++ pkg/control/waiter.go
@@ -1,7 +1,41 @@
 package control
 
+import (
+	"github.com/djkoelz/navi/pkg/comm"
+	"github.com/djkoelz/navi/pkg/service"
+	"time"
+)
+
 // should wait for an message to be posted
 // add a timeout feature, if the waiter timesout, we have failed the wait
 // otherwise success
 type Waiter struct {
+	desc   comm.Description
+	signal chan bool
+}
+
+func NewWaiter(d comm.Description) *Waiter {
+	w := new(Waiter)
+	w.desc = d
+	w.signal = make(chan bool)
+
+	return w
+}
+
+func (this *Waiter) callback(comm.Message) {
+	this.signal <- true
+}
+
+func (this *Waiter) Wait(d *service.Dispatcher, timeout time.Duration) bool {
+	d.Subscribe(this.desc, this.callback)
+
+	// wait with timeout until fails
+	select {
+	case <-this.signal:
+		return true
+	case <-time.After(timeout):
+		return false
+	}
+
+	return <-this.signal
 }
